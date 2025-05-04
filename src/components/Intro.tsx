b4a155eb3829c251bddab6aa33623fac1117231e
Leslie Mares <lesliemares@rightpoint.com>
2025-05-03 23:07:46 -0700

    feat: create intro

---
+++ src/components/Intro.tsx
@@ -0,0 +1,9 @@
+export default function Intro() {
+    return (
+    <div className="hero-banner">
+        <h1 className="hero-banner__title">Leslie Mares</h1>
+        <p className="hero-banner__subtitle">Software Developer</p>
+        <p className="hero-banner__motto">Crafting delightful interfaces and occasionally talking to my plants about them.</p>
+    </div>
+    );
+}
