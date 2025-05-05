477526fff0062ea227341d9ad4b85fbdf3909f09
Leslie Mares <lesliemares@rightpoint.com>
2025-05-05 13:53:23 -0700

    fix: turn footer into contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import ProjectList from "@/components/projects/ProjectList";
 import AboutMe from "@/components/AboutMe";
 import Skills from "@/components/Skills";
 import Intro from "@/components/Intro";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -21,54 +22,8 @@ export default function Home() {
         <AboutMe />
         <ProjectList />
         <Skills />
+        <Contact />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.linkedin.com/in/leslie-mares/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/linkedin.svg"
-            alt="LinkedIn icon"
-            width={16}
-            height={16}
-          />
-          LinkedIn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/lmaresRP"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/github.svg"
-            alt="GitHub icon"
-            width={16}
-            height={16}
-          />
-          GitHub
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="mailto:lmmares102@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/email.svg"
-            alt="Email icon"
-            width={16}
-            height={16}
-          />
-          Email
-        </a>
-      </footer>
     </div>
   );
 }
