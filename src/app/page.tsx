a0b28e94b57a76fae466dc13f0d3324bb0fc6f7d
Leslie Mares <lesliemares@rightpoint.com>
2025-05-05 14:00:09 -0700

    style: create brand colors and update main styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,8 @@ import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/LM-logo.png"
-          alt="Leslie Mares logo"
-          width={180}
-          height={38}
-          priority
-        />
-
+    <div className="">
+      <main className="">
         <Intro />
         <AboutMe />
         <ProjectList />
