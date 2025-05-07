a6360873acf753aa7cf655bd873ee589fa7a081d
Leslie Mares <lesliemares@rightpoint.com>
2025-05-07 15:46:49 -0700

    fix: update metadata

---
+++ src/app/layout.metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Leslie Mares | Frontend Developer",
+  description: "Frontend Developer specializing in React, Next.js, and modern web technologies. View my portfolio to see my latest projects and skills.",
+};
---
+++ src/components/header/MobileNav.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { scrollToElement } from '@/utils/scrollUtils';
+import ContactButton from './ContactButton';
+import Image from 'next/image';
+
+export default function MobileNav() {
+    return (
+        <div id="mobile-nav" className="mobile-nav flex flex-col p-3 bg-aths w-screen max-w-[300px] h-full">
+            <div className="mobile-nav__close flex justify-end pb-3">
+                <Image src="/close.svg" alt="close icon" width={30} height={30} />
+            </div>
+            <div className="mobile-nav__inner flex flex-col flex-grow gap-10 p-7 bg-white rounded-lg">
+                <div className="mobile-nav__links flex flex-col gap-8">
+                    <button
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-lunar font-semibold uppercase"
+                    onClick={() => scrollToElement('about')}
+                    type="button"
+                    >
+                        <Image src="/about.svg" alt="about icon" width={20} height={20} />
+                        About
+                    </button>
+                    <button
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-lunar font-semibold uppercase"
+                    onClick={() => scrollToElement('projects')}
+                    type="button"
+                    >
+                        <Image src="/projects.svg" alt="projects icon" width={20} height={20} />
+                        Projects
+                    </button>
+                    <button
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-lunar font-semibold uppercase"
+                    onClick={() => scrollToElement('skills')}
+                    type="button"
+                    >
+                        <Image src="/skills.svg" alt="skills icon" width={20} height={20} />
+                        Skills
+                    </button>
+                </div>
+                <ContactButton buttonStyle="primary" />
+            </div>
+        </div>
+    );
+}
