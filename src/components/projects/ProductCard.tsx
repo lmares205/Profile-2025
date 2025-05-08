6f41eb50bac781a1a050b491ad89e69aef1ad152
Leslie Mares <lesliemares@rightpoint.com>
2025-05-08 12:47:15 -0700

    feat: carousel markup, add images

--- src/components/projects/ProductCard.tsx
+++ src/components/projects/ProductCard.tsx
@@ -1,10 +1,44 @@
 import Image from "next/image";
 
-export default function ProductCard({ title, image, description }: { title: string; image: string; description: string }) {
+interface Image {
+    id: number;
+    src: string;
+    alt: string;
+}
+
+export default function ProductCard({ title, images, description, id }: { title: string; images: Image[]; description: string; id: number }) {
+    const imageCount = images.length;
     return (
     <div className="project-card">
-        <div className="project-card__image--wrapper">
-            <Image className="project-card__image" src={image} alt={title} width={150} height={150} />
+        <div className="flex flex-col" role="group" aria-roledescription="carousel" aria-label="Project Images">
+            <div className="flex flex-row gap-3 order-2" role="group" aria-label="Slide controls">
+                {Array.from({ length: imageCount }).map((_, i) => (
+                    <button
+                        key={i}
+                        type="button"
+                        aria-label={`Show slide ${i + 1} of ${imageCount}`}
+                        className="px-2"
+                        aria-current={i === 0 ? "true" : undefined}
+                    >
+                        <Image src='/navigation-dot.svg' alt="navigation dot" width={20} height={20} />
+                    </button>
+                ))}
+            </div>
+
+            <button aria-label="Previous slide">
+                <Image src={'/prev.svg'} alt="previous icon" width={15} height={15} />
+            </button>
+
+            {images.map((image) => (
+                <div key={image.id} role="group" aria-roledescription="slide" aria-labelledby={`carousel_item-${id}_heading`} aria-hidden={image.id === 1 ? "false" : "true"}>
+                    <h4 id={`carousel_item-${id}_heading`} aria-label={image.alt}></h4>
+                    <Image className="project-card__image" src={image.src} alt={image.alt} width={150} height={150} />
+                </div>
+            ))}
+
+            <button aria-label="Next slide">
+                <Image src={'/next.svg'} alt="previous icon" width={15} height={15} />
+            </button>
         </div>
         <div className="project-card__content--wrapper">
             <h3 className="project-card__title">{title}</h3>
--- src/components/projects/ProjectList.tsx
+++ src/components/projects/ProjectList.tsx
@@ -5,25 +5,79 @@ export default function ProjectList() {
         {
             id: 1,
             title: "Print and Digital Products and Services Client",
-            image: "/print.png",
+            images: [
+                {
+                    id: 1,
+                    src:"/print.png",
+                    alt: ""
+                },
+                {
+                    id: 2,
+                    src:"/print-2.png",
+                    alt: ""
+                },
+                {
+                    id: 3,
+                    src:"/print-3.png",
+                    alt: ""
+                }
+            ],
             description: "Led the development of a sophisticated product warranty management system, integrating dynamic warranty offerings across multiple product lines. Designed and implemented custom PHP modules to automate warranty associations, improve upsell opportunities, and enhance the customer experience by matching relevant warranty options to product attributes. Highlighted service agreements as add-ons throughout the user journey to increase product visibility, leveraging robust backend architecture to ensure scalability and maintainability, while adhering to SOLID principles. Built ADA-compliant elements, such as tooltips, radio buttons, and collapsible options, to further enhance the user experience.",
         },
         {
             id: 2,
             title: "Home Décor Industry Client",
-            image: "/home-decor.png",
+            images: [
+                {
+                    id: 1,
+                    src: "/home-decor.png",
+                    alt: ""
+                },
+                {
+                    id: 2,
+                    src: "/home-decor-2.png",
+                    alt: ""
+                },
+                {
+                    id: 3,
+                    src: "/home-decor-3.png",
+                    alt: ""
+                }
+            ],
             description: "Developed tailored user experiences for both retail and B2B users, including custom logic to distinguish and display sample vs. main products, with B2B-exclusive purchasing access and price visibility. Integrated APIs for dynamic custom rug pricing and implemented error handling for custom carpet dimensions. Modified third-party Klevu templates via post-render scripting to enhance product recommendations and designed a custom hero banner to elevate visual engagement—efforts that resulted in a 16% increase in conversion rate.",
         },
         {
             id: 3,
             title: "Boutique Fashion Industry Client",
-            image: "/boutique-fashion.png",
+            images: [
+                {
+                    id: 1,
+                    src: "/boutique-fashion.png",
+                    alt: ""
+                },
+                {
+                    id: 2,
+                    src: "/boutique-fashion-2.png",
+                    alt: ""
+                },
+                {
+                    id: 3,
+                    src: "/boutique-fashion-3.png",
+                    alt: ""
+                }
+            ],
             description: "Assisted in the full development of a Shopify store, from creating base theme styling to building custom content blocks and resolving bugs. Integrated Google Maps API to retrieve and display store location data based on proximity to users. Developed custom logic to link main products with child variants for individualized marketing and ranking. Designed ADA-compliant, responsive UI elements to ensure cross-browser compatibility, stability, and scalability. Aligned UI components with high-fidelity Figma designs and ensured quality by passing comprehensive functional QA testing. Shortly after launch, the site\'s first collaborative collection sold out in record time.",
         },
         {
             id: 4,
-            title: "headless eccomerce application",
-            image: "/boutique-fashion.png",
+            title: "Headless eCommerce Application",
+            images: [
+                {
+                    id: 1,
+                    src: "/boutique-fashion.png",
+                    alt: ""
+                }
+            ],
             description: "Led the development of a high-performance e-commerce solution using Shopify's Hydrogen framework and Remix, implementing a custom Yotpo Reviews integration that enhanced the product discovery experience. Engineered a secure, token-based authentication system with optimal caching strategies, resulting in improved API performance. Developed reusable TypeScript components adhering to modern web standards and accessibility guidelines. Utilized modern web technologies including React, TypeScript, GraphQL, and Tailwind CSS to create a responsive and maintainable codebase. The solution seamlessly integrated with Shopify's ecosystem while providing a superior user experience through custom-built features like star ratings, review statistics, and verified buyer badges."
         }
     ];
@@ -33,10 +87,24 @@ export default function ProjectList() {
             <h2 className="project-list__title">Projects</h2>
             <div className="project-list__wrapper flex flex-col gap-5">
                 {projects.map((project) => (
-                    <ProductCard key={project.id} title={project.title} image={project.image} description={project.description} />
+                    <ProductCard key={project.id} id={project.id} title={project.title} images={project.images} description={project.description} />
                 ))}
             </div>
         </div>
     </section>
     );
 }
+
+/*
+projects
+XER - pics
+ASH - pics
+LSF - pics
+
+Others projects- note to self make gif's
+BBB - pics
+CTV - pics
+NOU - pics
+TOR - pics
+REQ - pics
+*/
