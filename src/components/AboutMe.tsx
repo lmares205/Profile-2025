cc95ff3eb70f762280dd4f6a4f49f48a4509e5a8
Leslie Mares <lesliemares@rightpoint.com>
2025-05-07 22:24:56 -0700

    feat: about section styling

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,6 +1,6 @@
 export default function AboutMe() {
     return (
-    <section id="about" className="about-me">
+    <section id="about" className="about-me md:px-10 md:py-5 px-5 py-3 w-screen max-w-[1280px] mx-auto">
         <h2 className="about-me__title">About Me</h2>
         <div className="about-me__description">
             <p>I'm a software developer who loves building clean, intuitive interfaces that look great and work even better. I focus on front-end development and care a lot about making things accessible, responsive, and user-friendly. Before moving into tech, I worked in biotechnology and IT — industries where precision, compliance, and adaptability are key. That background really shaped how I approach problem-solving and taught me to deliver quality work under high standards. I’m all about turning ideas into real, usable experiences. When I’m not coding, you’ll probably find me in the garden, watering my plants and soaking up some fresh air.</p>
