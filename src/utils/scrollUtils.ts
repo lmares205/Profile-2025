cb910860c9a6560c4d281c79a7594dec8e6e5613
Leslie Mares <lesliemares@rightpoint.com>
2025-05-14 17:41:30 -0700

    fix: build

--- src/utils/scrollUtils.ts
+++ src/utils/scrollUtils.ts
@@ -1,12 +1,12 @@
 export const scrollToElement = (destination: string) => {
-    let destinationEl = document.getElementById(destination);
+    const destinationEl = document.getElementById(destination);
 
     if (destinationEl) {
         let headerOffset = 120;
         if(window.innerWidth < 768) {
             headerOffset = 100;
         } 
-        let destinationY = destinationEl.offsetTop - headerOffset;
+        const destinationY = destinationEl.offsetTop - headerOffset;
         window.scrollTo({
             top: destinationY,
             left: 0,
